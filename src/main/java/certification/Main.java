ad9f1f3cb762f937cef61ac456c8b92fab336bc7
SOUS021 <joao.vale@arvato.com>
2021-04-26 22:56:25 +0300

    'backup'

--- src/main/java/certification/Main.java
+++ src/main/java/certification/Main.java
@@ -1,13 +1,28 @@
 package certification;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = null;
-        Optional<String> optionalS = Optional.ofNullable(s);
-        Optional<String> optionalT = Optional.of(s);
+        int x = 0;
+        System.out.println(inc(++x));
+        System.out.println("x = " + x);
+        double d = 2.0;
+        float f = 1.0f;
+        long l = 23l;
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(l);
 
+        System.out.println(new String("teste").length());
+    }
+
+    private static int inc(int i) {
+        return i;
     }
 }
