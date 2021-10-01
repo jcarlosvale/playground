5036cf4cf7ea7a82adc0057aec70ec7153e6acdc
Joao Vale <jcarlos.vale@gmail.com>
2021-10-01 14:44:23 +0200

    leetcode

--- src/main/java/certification/Main.java
+++ src/main/java/certification/Main.java
@@ -1,16 +1,25 @@
 package certification;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.function.Supplier;
+
+class Employee{     int age;    }
 
 public class Main {
-
     public static void main(String[] args) {
-        SomeClassWithEnum.SomeEnum x = SomeClassWithEnum.SomeEnum.VALUE1;
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(0,0);
-        list.add(2,0);
-        System.out.println(list);
+
+        System.out.println(args.length);
+        var hasParams = (args == null ? false : true);
+        System.out.println(hasParams);
+
+        Employee e = new Employee();
+        Supplier<Employee> se = ()->{ e.age = 40; return e; }; //1
+        e.age = 50;//2
+        System.out.println(se.get().age); //3
+
+        String s = "       ";
+        System.out.println(s.isEmpty());
+        System.out.println(s.isBlank());
+        System.out.println(s.strip() != "");
+
     }
 }
