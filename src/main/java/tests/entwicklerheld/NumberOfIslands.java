b551bfa45bd041c485b757b48e0b3aed39815183
Joao Vale <jcarlos.vale@gmail.com>
2021-12-26 17:04:05 +0100

    number of islands

--- src/main/java/tests/entwicklerheld/NumberOfIslands.java
+++ src/main/java/tests/entwicklerheld/NumberOfIslands.java
@@ -3,24 +3,44 @@ package tests.entwicklerheld;
 public class NumberOfIslands {
     public static int numIslands(char[][] grid) {
         int count = 0;
+        int [][]visited = new int[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
+
                 int value = grid[i][j] - '0';
                 if (value == 1) {
-                    if (sumNeighbors(grid, i, j) == 0) { //border
-                        count++;
-                    }
+                     if (visited[i][j] != 1) {
+                         count++;
+                     }
+                     markConnections(grid, i, j, grid.length, grid[i].length, visited);
                 }
+                visited[i][j] = 1;
             }
         }
         return count;
     }
 
-    private static int sumNeighbors(char[][] grid, int i, int j) {
-        int leftValue = j == 0 ? 0 : grid[i][j-1] - '0';
-        int upValue = i == 0 ? 0 : grid[i-1][j] - '0';
-        return leftValue + upValue;
+    private static void markConnections(char[][] grid, int i, int j, int lineNum, int columnNum, int[][] visited) {
+        if (visited[i][j] != 1 && grid[i][j] == '1') {
+            visited[i][j] = 1;
+            //UP
+            if (i > 0) {
+                markConnections(grid, i-1, j, lineNum, columnNum, visited);;
+            }
+            //DOWN
+            if (i < lineNum-1) {
+                markConnections(grid, i+1, j, lineNum, columnNum, visited);;
+            }
+            //LEFT
+            if (j > 0) {
+                markConnections(grid, i, j-1, lineNum, columnNum, visited);;
+            }
+            //RIGHT
+            if (j < columnNum - 1) {
+                markConnections(grid, i, j+1, lineNum, columnNum, visited);;
+            }
+        }
     }
 
     public static void main(String[] args) {
