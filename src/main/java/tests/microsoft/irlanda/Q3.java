b3e1e9ebff9775cc180f06d5e2be0280f32a8d54
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-01-14 09:53:49 +0100

    microsoft failed

--- src/main/java/tests/microsoft/irlanda/Q3.java
+++ src/main/java/tests/microsoft/irlanda/Q3.java
@@ -1,11 +1,13 @@
 package tests.microsoft.irlanda;
 
-import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q3 {
     public static int solution(String S) {
-        int WEEK_MINUTES = 7 * 24 * 60;
-        boolean [] availabilityMinutes = new boolean[WEEK_MINUTES];
+        int LAST_MINUTES = 7 * 24 * 60;
+        List<Integer> meetings = new ArrayList<>();
+        meetings.add(0);
         //parse days
         for(String day : S.split("\n")) {
             int dayOfWeek = parseDayOfWeek(day.substring(0, 3).trim());
@@ -15,31 +17,20 @@ public class Q3 {
             int endMinute =  Integer.parseInt(day.substring(13,15));
 
             int startMeeting = dayOfWeek + (startHour * 60) + startMinute;
-            int endMeeting = dayOfWeek + (endHour * 60) + startMinute;
-            availabilityMinutes[startMeeting] = true;
-            availabilityMinutes[endMeeting] = true;
+            int endMeeting = dayOfWeek + (endHour * 60) + endMinute;
+
+            meetings.add(startMeeting);
+            meetings.add(endMeeting);
         }
+        meetings.add(LAST_MINUTES);
 
         //identify max space
         int maxRest = 0;
-        int counter = 0;
-        boolean busy = false;
-        for(boolean minutes: availabilityMinutes) {
-            if (minutes) {
-                if (!busy) {
-                    maxRest = Math.max(maxRest, counter);
-                    counter = 0;
-                }
-                busy = !busy;
-            } else {
-                if (!busy) counter++;
-            }
-        }
-        if (!busy) {
-            maxRest = Math.max(maxRest, counter);
+        for (int i = 0; i < meetings.size() - 1; i=i+2) {
+            maxRest = Math.max(meetings.get(i+1) - meetings.get(i), maxRest);
         }
 
-        return maxRest+1;
+        return maxRest;
     }
 
     private static int parseDayOfWeek(String dayOfWeek) {
@@ -57,6 +48,5 @@ public class Q3 {
     public static void main(String[] args) {
         System.out.println(solution("Mon 01:00-23:00\nTue 01:00-23:00\nWed 01:00-23:00\nThu 01:00-23:00\nFri 01:00-23:00\nSat 01:00-23:00\nSun 01:00-21:00")); //180
         System.out.println(solution("Sun 10:00-20:00\nFri 05:00-10:00\nFri 16:30-23:50\nSat 10:00-24:00\nSun 01:00-04:00\nSat 02:00-06:00\nTue 03:30-18:15\nTue 19:00-20:00\nWed 04:25-15:14\nWed 15:14-22:40\nThu 00:00-23:59\nMon 05:00-13:00\nMon 15:00-21:00")); //505
-        System.out.println(solution("")); //505
     }
 }
