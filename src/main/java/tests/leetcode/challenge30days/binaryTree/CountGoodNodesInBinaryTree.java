0f7e60e761e17ac956003a758c42cbe9ff8b1c9a
jcarlos.vale <jcarlos.vale@gmail.com>
2022-09-01 15:16:06 +0100

    link

--- src/main/java/tests/leetcode/challenge30days/binaryTree/CountGoodNodesInBinaryTree.java
+++ src/main/java/tests/leetcode/challenge30days/binaryTree/CountGoodNodesInBinaryTree.java
@@ -2,6 +2,7 @@ package tests.leetcode.challenge30days.binaryTree;
 
 import tests.leetcode.challenge30days.dataStructure.TreeNode;
 /**
+ * https://leetcode.com/problems/count-good-nodes-in-binary-tree/
  * Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
  *
  * Return the number of good nodes in the binary tree.
