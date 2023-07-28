782b5b105b49c8832ec3abbe3e9e3a156f4d645e
Joao Vale <jcarlos.vale@gmail.com>
2023-07-28 11:34:11 +0100

    teste

---
+++ src/main/java/tests/number8/Teste.java
@@ -0,0 +1,10 @@
+package tests.number8;
+
+import java.time.LocalDateTime;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+    }
+}
